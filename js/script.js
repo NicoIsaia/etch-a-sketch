ef406c1e70b013f72756329378a24175133dfdb6
Nico <nicoisaia@outlook.com>
2023-02-02 21:21:35 -0300

    Completed basic etch-a-sketch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-const container = document.querySelector("#container");
-
-
-for (i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.style.cssText = "height: 8px; width: 8px; background-color: gray;";
-    container.appendChild(square);
+let size = 40;
+const canvas = 360;
+if (localStorage.getItem("size")) {
+    size = localStorage.getItem("size");
 }
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let tempSize = parseInt(prompt("Please enter the size of the grid in one number"));
+    if (tempSize < 100 && tempSize > 0) {
+        localStorage.setItem("size", tempSize);
+        window.location.reload();
+    } else {
+        alert("Size must be between 0 and 100");
+    }
+});
+
+const container = document.querySelector("#container");
+container.setAttribute('style', `width: ${canvas}px; height: ${canvas}px;`);
+
+for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.cssText = `height: ${canvas/size}px; width: ${canvas/size}px; background-color: gray;`;
+        container.appendChild(square);
+    }
+}
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((sq) => {
+    sq.addEventListener('mouseover', () => {
+        sq.style.cssText = `height: ${360/size}px; width: ${360/size}px; background-color: white;`;
+    })
+});
+
