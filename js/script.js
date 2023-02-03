8f5fadfed09f19e855838a8634d56f4ee528312a
Nico <nicoisaia@outlook.com>
2023-02-03 18:39:53 -0300

    Added class to sq to start the changing colors assignment

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ for (i = 0; i < size; i++) {
     for (j = 0; j < size; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.classList.add('blank');
         square.style.cssText = `height: ${canvas/size}px; width: ${canvas/size}px; background-color: gray;`;
         container.appendChild(square);
     }
@@ -32,6 +33,16 @@ const squares = document.querySelectorAll('.square');
 squares.forEach((sq) => {
     sq.addEventListener('mouseover', () => {
         sq.style.cssText = `height: ${canvas/size}px; width: ${canvas/size}px; background-color: white;`;
+        /* Can't use "Array.includes()" directly because classList is an 
+        "arraylike object" (in this case a DOMTokenList) that doesn't have the 
+        same methods as a regular array.
+        In this case I use the "spread operator" between [] to make an array.
+        I could use the DOMTokenList method .contains() but as I'm still learning
+        I think it would be best to remember the [...] method to create arrays*/
+        if ([...sq.classList].includes('blank')) {
+            console.log('It was blank');
+            sq.classList.remove('blank');
+        }
     })
 });
 
